d78461d0583ebd9fb7c65a75434240b96aa61634
miyant <tsutomu-miyano@alpsgiken.co.jp>
2017-10-29 15:58:59 +0900

    ファイル追加されていなかったので追加（ビルド対策）

---
+++ GccApplication1/TurnManager.h
@@ -0,0 +1,22 @@
+/*
+ * TurnManager.h
+ *
+ * Created: 2017/10/28 22:55:59
+ *  Author: Administrator
+ */ 
+
+
+#ifndef TURNMANAGER_H_
+#define TURNMANAGER_H_
+
+
+// ------------------ Defined ------------------
+
+
+// ------------------ Global variables ------------------
+
+
+// ------------------ Method Definition ------------------
+
+
+#endif /* TURNMANAGER_H_ */
